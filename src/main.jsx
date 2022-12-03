d077d4724cbd6611328f8a80223a4e9e1abb3789
Marcos Diaz <progmarcosdiaz2022@gmail.com>
2022-12-02 21:36:19 -0500

    error with deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import 'bootswatch/dist/Superhero/bootstrap.min.css'; // Added this :boom:
+import 'bootswatch/dist/superhero/bootstrap.min.css'; // Added this :boom:
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
